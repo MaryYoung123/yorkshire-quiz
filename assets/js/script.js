c40e0d443cafa40f2cf236d8425c0a0d19a63374
Mary Young <itsmaryyyyy@gmail.com>
2024-06-11 16:48:42 +0000

    add different messages depending on users score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const questions = [
-    
-    
+
+
     {
         question: "The Great Yorkshire Show runs for three days each year, but in what month?",
         choices: ["June", "July", "August"],
@@ -20,7 +20,7 @@ const questions = [
         question: " Which date is designated Yorkshire day?",
         choices: ["February 14th", "March 30th", "August 1st"],
         correct: 2
-        },
+    },
     {
         question: " In which Yorkshire city were the Kaiser Chiefs formed?",
         choices: ["Leeds", "Bradford", "York"],
@@ -31,28 +31,33 @@ const username = sessionStorage.getItem('playerName');
 const quiz = document.getElementById("quiz");
 let currentQuestion = 0;
 let correctAnswers = 0;
+
 function checkAnswer(selected) {
     const feedback = document.getElementById("feedback");
     let currentCorrectAnswer = questions[currentQuestion].correct
     if (selected === questions[currentQuestion].correct) {
-        feedback.textContent = "Correct!";
+        feedback.textContent = "By 'eck, tha's reet!";
         correctAnswers++;
     } else {
-        feedback.textContent = `Incorrect! the correct answer is ${questions[currentQuestion].choices[currentCorrectAnswer]}`  ;
+        feedback.textContent = `nah, the correct answer is ${questions[currentQuestion].choices[currentCorrectAnswer]}`;
     }
     setTimeout(() => {
         currentQuestion++;
         if (currentQuestion < questions.length) {
             showQuestion();
+        } else if (correctAnswers >= 3) {
+            const gameArea = document.querySelector(".game-area");
+            gameArea.innerHTML = `<p> Ey up chuck! You got ${correctAnswers} out of ${questions.length} questions. Chuffed!</p>`;
         } else {
-            const gameArea = document.querySelector(".game-area"); 
-            gameArea.innerHTML = `<p>You got ${correctAnswers} out of ${questions.length} questions.</p>`;
+            const gameArea = document.querySelector(".game-area");
+            gameArea.innerHTML = `<p> Daft as a brush! You got ${correctAnswers} out of ${questions.length} questions. Be reet.</p>`;
         }
     }, 2000);
 }
+
 function showQuestion() {
-    quiz.style.display = 'block'; 
-    
+    quiz.style.display = 'block';
+
     const questionText = document.getElementById("question-text");
     questionText.textContent = questions[currentQuestion].question;
     const choices = document.querySelectorAll(".choice");
@@ -66,20 +71,3 @@ document.addEventListener('DOMContentLoaded', function () {
     alert(`Welcome to the quiz ${username}`);
     showQuestion();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
